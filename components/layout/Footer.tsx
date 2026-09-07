15301fe2039f552f5860fcc7aa31814a3e71c336
BizTradeFairs <biztradefairs.com@gmail.com>
2026-09-07 13:53:39 +0530

    update

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import Container from '../ui/container';
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-gray-400 border-t border-white/10">
+    <footer className="bg-[#03193D] text-gray-400 border-t border-white/10">
       {/* Top Section */}
       <Container className="py-16 lg:py-20">
         <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-4 xl:gap-20">
