281c7d0c2257e447988c13d8bda344a77705365d
Sandeep1816 <avalasandeep02@gmail.com>
2026-09-21 14:40:43 +0530

    hero color

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -27,7 +27,7 @@ export default function HeroSection() {
   return (
     <section className="relative flex h-[calc(100svh-var(--mobile-header-height,110px))] w-full items-end justify-center overflow-hidden text-white lg:h-screen">
       {/* Gradient overlay */}
-      <div className="absolute inset-0 z-[-1] bg-gradient-to-t from-black via-black/50 to-transparent" />
+      <div className="absolute inset-0 z-[-1] bg-gradient-to-t from-[#031A34] via-[#031A34]/50 to-transparent" />
 
       {/* Slideshow background */}
       <div className="absolute inset-0 z-[-2] size-full bg-[#031A34]">
